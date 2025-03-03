24c78178308d08afc0e9245dd3843c0160e679a5
MIKHAIL KOUTUN <s29883@pjwstk.edu.pl>
2025-03-03 11:14:46 +0100

    Add function printNums

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+
 Console.WriteLine("Hello, World!");
+
+
+void printNums() {
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+printNums();
