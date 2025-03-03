143a2799dcb0340e63a9a7f8bf3e54fc60459a38
MIKHAIL KOUTUN <s29883@pjwstk.edu.pl>
2025-03-03 11:18:42 +0100

    Add class Car

--- Tutorial1/Tutorial1/Program.cs
+++ Tutorial1/Tutorial1/Program.cs
@@ -12,3 +12,18 @@ void printNums() {
 }
 
 printNums();
+Car car = new Car("Opel", 15123123);
+Console.WriteLine(car.model);
+
+class Car
+{
+    public string model;
+    public int weight;
+
+    public Car(string model, int weight)
+    {
+        this.model = model;
+        this.weight = weight;
+    }
+}
+
